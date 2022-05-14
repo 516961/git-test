8cf7caa4fb4d0430fdbaf778af6708004a4bb578
linghuchong <aa312312@qq.com>
2022-05-14 15:09:11 +0800

    Update ja.java

--- src/main/java/com/chen/ja.java
+++ src/main/java/com/chen/ja.java
@@ -3,7 +3,7 @@ package com.chen;
 public class ja {
     public static void main(String[] args) {
         System.out.printf("hello git");
-        System.out.printf("hello git1");
+        System.out.printf("hello git11");
         System.out.printf("hello git2 hot fix final");
     }
 }
