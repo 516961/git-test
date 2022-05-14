d8f74961704f9a522f18a9bcb2b334b28a5eee44
Chen <Chen@Chen.com>
2022-05-14 15:19:16 +0800

    haha

--- src/main/java/com/chen/ja.java
+++ src/main/java/com/chen/ja.java
@@ -5,5 +5,6 @@ public class ja {
         System.out.printf("hello git");
         System.out.printf("hello git11");
         System.out.printf("hello git2 hot fix final");
+        System.out.printf("hello git2 push");
     }
 }
